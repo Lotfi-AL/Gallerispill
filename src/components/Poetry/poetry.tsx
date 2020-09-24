18c86ece94fe720148cd0b912b6adfbdba95e7db
Sepehr <43407185+sepehrn0107@users.noreply.github.com>
2020-09-24 14:58:54 +0200

    added highlighter, not working

--- src/components/Poetry/poetry.tsx
+++ src/components/Poetry/poetry.tsx
@@ -25,12 +25,43 @@ export default class Poetry extends Component<{}, PoetryDB> {
             lines: data[0].lines 
         })  
         
-         
-    };
 
+    };
+    highlightPoem() {
+        console.log("fungerer dette?")
+        let currentLine = 0;
+        console.log("listelengde" + this.state.lines.length);
+        const lineTimer = setInterval(() => {
+            if(currentLine > this.state.lines.length) {
+                clearInterval(lineTimer);
+            }
+
+            currentLine += 1;
+            console.log(currentLine);
+            const id = document.querySelector("#" + currentLine); 
+            id.className += "word";
+
+        }, 2000)
+
+
+        /*
+        this.state.lines.forEach((s, i) => {
+            console.log("beginning loop")
+            setTimeout(() => {
+                const id = document.getElementById(String(i)); 
+
+                id.className += "word";
+            }, i * 5000)
+            console.log("after loop")
+            
+
+        })*/
+
+    }
     
     componentDidMount(){
-        this.getPoem("George Eliot", "God Needs Antonio");
+        this.getPoem("George Eliot", "God Needs Antonio").then(() => {this.highlightPoem()});
+        
     }
     render() {
         if (!this.state) {
@@ -41,7 +72,7 @@ export default class Poetry extends Component<{}, PoetryDB> {
         
         console.log(this.state.author)
         const items = this.state.lines.map((line, index) => {
-            return <p key={index}>{line}</p>
+            return <p key={index} id={String(index)}><span data-text={line}>{line}</span></p>
         })
 
         return (
@@ -51,6 +82,7 @@ export default class Poetry extends Component<{}, PoetryDB> {
                 <h2>{this.state.title}</h2>
                 <div className="poemLines">
                     {items}
+                    
                 </div>
             </div>
             
