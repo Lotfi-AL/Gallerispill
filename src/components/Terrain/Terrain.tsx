f728d75015b8366524aa61e554d4be025a2bc12e
sepehr nasiri <sepehrn@stud.ntnu.no>
2020-09-27 23:14:47 +0200

    started working on rain and wind animation

--- src/components/Terrain/Terrain.tsx
+++ src/components/Terrain/Terrain.tsx
@@ -2,23 +2,40 @@ import React, { useEffect } from "react";
 import skyTexture from "../../assets/terrain/sky_fc.png";
 import farMountainsTexture from "../../assets/terrain/far_mountains_fc.png";
 import grassyMountainsTexture from "../../assets/terrain/grassy_mountains_fc.png";
+import rainTexture from "../../assets/terrain/rain.png";
 import "./Terrain.css";
 import Cloud from "./Cloud";
+import { useStatus } from "../Store/StatusProvider";
 
 const Terrain = () => {
-    let clouds: JSX.Element[] = [];
+    const { status } = useStatus();
+    const clouds: JSX.Element[] = [];
     const AddCloud = (min: number, max: number) => {
         for (let i = 0; i <= Math.floor(Math.random() * (max - min + 1) + min); i++) {
             clouds.push(<Cloud />);
         }
     };
-    AddCloud(3, 6);
+    const Rain = () => {
+        if (status.rain == true) {
+            console.log("YESSSSSSS");
+            const rainDrop: JSX.Element[] = [];
+            for (let j = 0; j < 300; j++) {
+                rainDrop.push(<i className="rain"></i>);
+            }
 
+            return <div className="rainContainer">{rainDrop}</div>;
+        } else return null;
+    };
+
+    AddCloud(3, 6);
     return (
-        <div className="skybox day">
-            <div className="cloud one">{clouds}</div>
-            <div className="cloud two">{clouds}</div>
-        </div>
+        <>
+            <Rain></Rain>
+            <div className="skybox day">
+                <div className="cloud one">{clouds}</div>
+                <div className="cloud two">{clouds}</div>
+            </div>
+        </>
     );
 };
 
