49c9e878e1967d0176233b35280625f96436c709
lotfi-AL <lotfi.amin@hotmail.com>
2020-09-28 17:01:30 +0200

    fixed bug with local storage

--- src/components/Landingpage/Landingpage.tsx
+++ src/components/Landingpage/Landingpage.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./Landingpage.css";
 import "./Animation.css";
-import { LoadBtn } from "../ControlPanel/LoadBtn";
+import { LoadBtn } from "./LoadBtn";
 
 interface StatusCardProps {
     DefaultStatus: StatusTypes;
@@ -10,39 +10,34 @@ interface StatusCardProps {
 interface StatusCardState {
     Status: StatusTypes;
 }
-interface ToggleProps {
+export interface ToggleProps {
     ClickHandler: () => void;
 }
 
 type StatusTypes = Boolean;
 
-export default class Landingpage extends React.Component<ToggleProps, {}> {
-    constructor(props: any) {
-        super(props);
-    }
-    render() {
-        return (
-            <div className="landingpage">
-                <div className="instructions box">
-                    <h1>Instruksjoner</h1>
-                    <p>What we do noobs</p>
-                </div>
-                <div className="credits">
-                    <div className="container box">
-                        <p className="text">Created by</p>
+export default function Landingpage(props: ToggleProps) {
+    return (
+        <div className="landingpage">
+            <div className="instructions box">
+                <h1>Instruksjoner</h1>
+                <p>What we do noobs</p>
+            </div>
+            <div className="credits">
+                <div className="container box">
+                    <p className="text">Created by</p>
 
-                        <ul className="list">
-                            <li className="item">Amund Lunke Røhne</li>
-                            <li className="item">Lotfi Amin Lazreg</li>
-                            <li className="item">Sepehr Nasiri Behrouz</li>
-                        </ul>
-                    </div>
-                </div>
-                <LoadBtn></LoadBtn>
-                <div onClick={this.props.ClickHandler} className="box link startLink">
-                    Click to start!
+                    <ul className="list">
+                        <li className="item">Amund Lunke Røhne</li>
+                        <li className="item">Lotfi Amin Lazreg</li>
+                        <li className="item">Sepehr Nasiri Behrouz</li>
+                    </ul>
                 </div>
             </div>
-        );
-    }
+            <LoadBtn ClickHandler={props.ClickHandler}></LoadBtn>
+            <div onClick={props.ClickHandler} className="box link startLink">
+                Click to start!
+            </div>
+        </div>
+    );
 }
