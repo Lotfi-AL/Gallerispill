ed9369b3d14ccdb0ad6a0f6b1c4ca1c6c76f7408
lotfi-AL <lotfi.amin@hotmail.com>
2020-09-28 17:36:43 +0200

    Changed some css on the landingpage, and added credits and instructions closes #1

--- src/components/Landingpage/Landingpage.tsx
+++ src/components/Landingpage/Landingpage.tsx
@@ -20,23 +20,84 @@ export default function Landingpage(props: ToggleProps) {
     return (
         <div className="landingpage">
             <div className="instructions box">
-                <h1>Instruksjoner</h1>
-                <p>What we do noobs</p>
+                <h1>Description</h1>
+                <p>
+                    This is a 2d immersive poem experience. Move with arrow keys and interact with scene components
+                    using the spacebar. Read along with the poems as they are highlighted and go through the doors to
+                    switch poem and scene. As you go along you can change the scene to your liking by interacting with
+                    the paintings.
+                </p>
             </div>
             <div className="credits">
                 <div className="container box">
                     <p className="text">Created by</p>
 
                     <ul className="list">
-                        <li className="item">Amund Lunke Røhne</li>
-                        <li className="item">Lotfi Amin Lazreg</li>
-                        <li className="item">Sepehr Nasiri Behrouz</li>
+                        <li className="item">Yeti</li>
+                        <li className="item">Hobbit</li>
+                        <li className="item">Gorilla</li>
                     </ul>
                 </div>
             </div>
             <LoadBtn ClickHandler={props.ClickHandler}></LoadBtn>
             <div onClick={props.ClickHandler} className="box link startLink">
                 Click to start!
+            </div>
+            <div className="box">
+                <h2>Assets and sources</h2>
+                <ul>
+                    <li>
+                        <a target="_blank" href="">
+                            Poetry
+                        </a>
+                    </li>
+                    <li>
+                        SFX
+                        <ul>
+                            <li>
+                                <a
+                                    target="_blank"
+                                    href="https://www.fesliyanstudios.com/royalty-free-sound-effects-download/rain-21">
+                                    Rain
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    target="_blank"
+                                    href="https://www.fesliyanstudios.com/royalty-free-sound-effects-download/wind-blowing-37">
+                                    Wind
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a target="_blank" href="https://oddpotatogift.itch.io/16x16-fantasy-pack">
+                            House tileMap
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="https://thkaspar.itch.io/micro-character-bases">
+                            Character
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="https://vnitti.itch.io/grassy-mountains-parallax-background">
+                            Terrain
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target="_blank"
+                            href="https://fonts.google.com/specimen/Press+Start+2P?sidebar.open=true&selection.family=Press+Start+2P">
+                            Fonts
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="https://codepen.io/yoannhel/pen/sJpDj">
+                            Credits animation
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     );
