d96ef1715b93a16458c897ebd15220eb014632c1
r33an1 <r33an1@me.com>
2020-09-28 22:50:37 +0200

    Jest config

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import { App } from "../components/App";
+
+describe("<App />", () => {
+    test("should display a blank login form, with remember me checked by default", async () => {
+        // ???
+    });
+});
